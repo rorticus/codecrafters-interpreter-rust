c5ed43019aef27be26f43332014393c32673288c
Rory Mulligan <rorticus@gmail.com>
2026-06-19 16:02:44 -0400

    codecrafters submit [skip ci]

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -32,6 +32,11 @@ impl Interpreter {
             Expr::Literal(literal) => Ok(Value::from_literal(literal)),
             Expr::Grouping(expr) => self.evaluate(expr),
             Expr::Unary { operator, right } => self.eval_unary(operator, right),
+            Expr::Binary {
+                left,
+                operator,
+                right,
+            } => self.eval_binary(left, operator, right),
             _ => Err(InterpreterError::UnhandledException),
         }
     }
@@ -44,6 +49,27 @@ impl Interpreter {
             TokenKind::Minus => Ok(Value::Number(0f64 - value.as_number())),
             _ => Err(InterpreterError::Internal(format!(
                 "Unhandled unary {}",
+                operator.lexeme
+            ))),
+        }
+    }
+
+    fn eval_binary(
+        &self,
+        left: &Expr,
+        operator: &Token,
+        right: &Expr,
+    ) -> Result<Value, InterpreterError> {
+        let left = self.evaluate(left)?;
+        let right = self.evaluate(right)?;
+
+        match operator.kind {
+            TokenKind::Star => Ok(Value::Number(left.as_number() * right.as_number())),
+            TokenKind::Slash => Ok(Value::Number(left.as_number() / right.as_number())),
+            TokenKind::Plus => Ok(Value::Number(left.as_number() + right.as_number())),
+            TokenKind::Minus => Ok(Value::Number(left.as_number() - right.as_number())),
+            _ => Err(InterpreterError::Internal(format!(
+                "Unhandled binary operation {}",
                 operator.lexeme
             ))),
         }
